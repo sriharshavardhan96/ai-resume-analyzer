f02cd4615bd583708b9530276294de0f298c4687
shv-peri <harsha.vardhan@vation.com>
2025-12-27 14:11:47 +0530

    resume feedback components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -73,7 +73,7 @@ const resume = () => {
                 {feedback ? (
                     <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                        <Summary feedback={feedback} />
-                       <ATS score={feedback.ATS.score || 0} suggestions = {feedback.padStart.tips || []} />
+                       <ATS score={feedback.ATS.score || 0} suggestions = {feedback.ATS.tips || []} />
                        <Details feedback={feedback} />
                     </div>
                 ) : (
